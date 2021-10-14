c9d326d3a4bf89eb23a6226c121eb0e1a44e96e1
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-10-14 20:42:14 +0530

    Add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
       <Head>
         <title>Amazon Clone</title>
         <meta name="description" content="Amazon Clone" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
 
       <Header />
