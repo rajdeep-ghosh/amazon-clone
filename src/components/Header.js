6cf60022a22af7c7ae9d941674c2d973685e4c5f
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-10-19 21:07:17 +0530

    Add top nav right container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import amznLogoREV from "../assets/images/amzn-logo-rev.svg";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
 
 function Header() {
   return (
@@ -18,10 +18,28 @@ function Header() {
             className="cursor-pointer"
           />
         </div>
+        
         {/* Search */}
         <div className="hidden sm:flex flex-grow items-center h-10 rounded-md bg-yellow-400 hover:bg-yellow-500 cursor-pointer">
           <input className="px-4 py-2 h-full w-6 flex-1 rounded-l-md focus:outline-none" type="text" placeholder="Search" />
           <SearchIcon className="h-8 p-2" />
+        </div>
+
+        {/* Right container */}
+        <div className="text-white text-xs flex items-center space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Rajdeep Ghosh</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns &</p>
+            <p className="font-extrabold md:text-sm">Orders</p>
+          </div>
+          <div className="link relative flex items-center space-x-1">
+            <span className="absolute -top-1 -right-1 md:right-11 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+            <ShoppingCartIcon className="h-8" />
+            <p className="hidden md:inline mt-2 font-extrabold md:text-sm">Basket</p>
+          </div>
         </div>
       </div>
     </header>
