5c1f4d039daff15856915c8346b7b0cadf132e35
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-10-20 11:48:10 +0530

    Add more carousel

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,14 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Banner() {
   const carouselImages = [
-    { index: "1", url: "https://bit.ly/3aTtuul", name: "Prime Video" },
-    { index: "2", url: "https://bit.ly/3aUWWjK", name: "Amazon Music" },
-    { index: "3", url: "https://bit.ly/3DWPVeF", name: "Audible" },
+    { index: "1", url: "https://bit.ly/2YYUbLr", name: "Index" },
+    { index: "2", url: "https://bit.ly/3aTtuul", name: "Prime Video" },
+    { index: "6", url: "https://bit.ly/3lVrdFe", name: "Fashion" },
+    { index: "4", url: "https://bit.ly/3aUWWjK", name: "Amazon Music" },
+    { index: "3", url: "https://bit.ly/3Ge4RHp", name: "Books & Games" },
+    { index: "7", url: "https://bit.ly/2Z3swJt", name: "Home & kitchen" },
+    { index: "8", url: "https://bit.ly/2ZbLduD", name: "Gromming" },
+    { index: "5", url: "https://bit.ly/3DWPVeF", name: "Audible" },
   ];
 
   return (
@@ -15,7 +20,7 @@ function Banner() {
         showStatus={false}
         showThumbs={false}
         showIndicators={true}
-        interval={5000}
+        interval={4000}
         transitionTime={200}
         autoPlay
         infiniteLoop
