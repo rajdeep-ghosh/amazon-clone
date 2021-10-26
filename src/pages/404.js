1ab076e3b7d78488c81030bad4f5d3aa5ea2501e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-10-26 22:27:28 +0530

    Create custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+function custom404() {
+  return (
+    <div>
+      <div>404</div>
+      <div>
+        <h1>Page not found</h1>
+        <p>The page you are looking for doesn&apos;t exist or has been moved.</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default custom404;
