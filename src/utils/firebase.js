08dc3116d4cd0b41e9fb688376f608b95fd2c4e1
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-11-05 05:11:17 +0530

    Add firebase config

---
+++ src/utils/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChEV8T4VRBflD_X3q2CpuLYD1rLhH8GM4",
+  authDomain: "amzn-clone-57bdf.firebaseapp.com",
+  projectId: "amzn-clone-57bdf",
+  storageBucket: "amzn-clone-57bdf.appspot.com",
+  messagingSenderId: "506484520607",
+  appId: "1:506484520607:web:82c87218db0e0f371194aa",
+};
