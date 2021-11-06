2a141a8479ea87790c1f20aa9a77fbe17cabce6a
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-11-07 03:23:24 +0530

    Setup checkout route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import amznLogoREV from "../assets/images/amzn-logo-rev.svg";
-import { SearchIcon, ShoppingCartIcon, MenuIcon } from "@heroicons/react/outline";
+import {
+  SearchIcon,
+  ShoppingCartIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 import indiaFlag from "../assets/images/india-flag.svg";
 
 function Header() {
   const [session] = useSession();
+  const router = useRouter();
 
   return (
     <header>
@@ -14,6 +20,7 @@ function Header() {
         {/* Logo */}
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => {router.push("/")}}
             src={amznLogoREV}
             width={140}
             height={30}
@@ -22,23 +29,27 @@ function Header() {
             className="cursor-pointer"
           />
         </div>
-        
+
         {/* Search */}
         <div className="hidden sm:flex flex-grow items-center h-10 rounded-md bg-yellow-400 hover:bg-yellow-500 cursor-pointer">
-          <input className="px-4 py-2 h-full w-6 flex-1 rounded-l-md focus:outline-none" type="text" placeholder="Search" />
+          <input
+            className="px-4 py-2 h-full w-6 flex-1 rounded-l-md focus:outline-none"
+            type="text"
+            placeholder="Search"
+          />
           <SearchIcon className="h-8 p-2" />
         </div>
 
         {/* Right container */}
         <div className="text-white text-xs flex items-center space-x-6 mx-6 whitespace-nowrap">
-          <Image 
-            src={indiaFlag} 
+          <Image
+            src={indiaFlag}
             height={30}
-            width={30} 
-            alt="Country flag" 
-            objectFit="contain" 
-            className="hidden md:inline cursor-pointer" 
-          />          
+            width={30}
+            alt="Country flag"
+            objectFit="contain"
+            className="hidden md:inline cursor-pointer"
+          />
           <div onClick={!session ? signIn : signOut} className="link">
             <p>{session ? `Hello, ${session.user.name}` : `Sign In`}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
@@ -47,10 +58,14 @@ function Header() {
             <p>Returns &</p>
             <p className="font-extrabold md:text-sm">Orders</p>
           </div>
-          <div className="link relative flex items-center space-x-1">
-            <span className="absolute -top-1 -right-1 md:right-11 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+          <div onClick={() => {router.push("/checkout")}} className="link relative flex items-center space-x-1">
+            <span className="absolute -top-1 -right-1 md:right-11 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
             <ShoppingCartIcon className="h-8" />
-            <p className="hidden md:inline mt-1 font-extrabold md:text-sm">Basket</p>
+            <p className="hidden md:inline mt-1 font-extrabold md:text-sm">
+              Basket
+            </p>
           </div>
         </div>
       </div>
