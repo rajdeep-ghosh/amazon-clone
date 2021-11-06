5eea0d2e7964db9329abdf138ad35d68c50cb3e1
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-11-07 03:52:34 +0530

    Populate left section of checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,9 +1,30 @@
+import Image from "next/image";
 import Header from "../components/Header";
 
 function Checkout() {
   return (
-    <div>
+    <div className="bg-gray-100 font-Nunito-Sans antialiased">
       <Header />
+
+      <main className="lg:flex max-w-screen-xl mx-auto">
+        {/* Left Section */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://bit.ly/3kfRLjc"
+            alt="checkout_banner"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+
+          <div className="p-5 bg-white">
+            <h1 className="text-3xl font-semibold border-b-2 pb-4">Your Shopping Basket</h1>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div></div>
+      </main>
     </div>
   );
 }
