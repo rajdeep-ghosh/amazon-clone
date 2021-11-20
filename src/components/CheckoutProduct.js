4f9b18959293f937b7b5ab38f28ea9e668018829
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-11-20 23:40:49 +0530

    Make checkout page bit responsive

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -4,7 +4,7 @@ import Currency from "react-currency-formatter";
 
 function CheckoutProduct(props) {
   return (
-    <div className="grid grid-cols-5 mb-8">
+    <div className="grid md:grid-cols-5 mb-8">
       <Image
         src={props.img}
         width={200}
@@ -13,22 +13,22 @@ function CheckoutProduct(props) {
         objectFit="contain"
       />
 
-      <div className="col-span-3 flex flex-col space-y-1 mx-5">
-        <h3 className="font-semibold text-lg line-clamp-1">{props.title}</h3>
+      <div className="md:col-span-3 flex flex-col space-y-1 mx-5 mt-4">
+        <h3 className="font-medium md:font-semibold md:text-lg line-clamp-1">{props.title}</h3>
         <div className="flex items-center space-x-2">
           <StarIcon className="h-4 text-yellow-500" />
-          <p>
-            {props.rating.rate} <span className="text-sm text-gray-400">({props.rating.count})</span>
+          <p className="text-sm md:text-base">
+            {props.rating.rate} <span className="text-xs md:text-sm text-gray-400">({props.rating.count})</span>
           </p>
         </div>
-        <p className="text-sm line-clamp-3 texts">{props.desc}</p>
+        <p className="text-sm line-clamp-3">{props.desc}</p>
         <div className="text-lg font-semibold">
           <Currency quantity={props.price} currency="USD" />
         </div>
         {props.hasPrime && (
           <div className="flex items-center space-x-2">
             <Image src="https://bit.ly/3prP2qx" width={48} height={48} alt="prime" />
-            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+            <p className="hidden md:block text-xs text-gray-500">FREE Next-day Delivery</p>
           </div>
         )}
       </div>
